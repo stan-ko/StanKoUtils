63e0b3b8a53e3a8a521df5f667d25ef779c6083a
stan <koshutsky@rambler.ru>
2016-08-12 12:55:35 +0300

    Changed LocaleHelper

--- app/src/main/java/com/stanko/tools/LocaleHelper.java
+++ app/src/main/java/com/stanko/tools/LocaleHelper.java
@@ -4,6 +4,8 @@ import java.util.Locale;
 
 import android.content.Context;
 import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.util.DisplayMetrics;
 
 public class LocaleHelper {
 	
@@ -17,9 +19,14 @@ public class LocaleHelper {
 
 	public static void setAppLocale(Context context, final Locale locale2set) {
 		Locale.setDefault(locale2set);
-		Configuration config = new Configuration();
-		config.locale = locale2set;
-		context.getApplicationContext().getResources().updateConfiguration(config, context.getApplicationContext().getResources().getDisplayMetrics());
+		final Resources resources = context.getResources();
+		final DisplayMetrics displayMetrics = resources.getDisplayMetrics();
+		final Configuration configuration = resources.getConfiguration();
+		configuration.locale = locale2set;
+		resources.updateConfiguration(configuration, displayMetrics);
+//		Configuration config = new Configuration();
+//		config.locale = locale2set;
+//		context.getApplicationContext().getResources().updateConfiguration(config, context.getApplicationContext().getResources().getDisplayMetrics());
 	}
 
 }
